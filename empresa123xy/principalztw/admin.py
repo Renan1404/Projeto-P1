e1a367b40fa9313e0a1be20c050eeffe7566fce9
renan1404 <re-nan-1404@hotmail.com>
2022-10-25 21:29:07 -0300

    Projeto-P1

--- empresa123xy/principalztw/admin.py
+++ empresa123xy/principalztw/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Voos, Hotel
 
+admin.site.register(Voos)
+admin.site.register(Hotel)
 # Register your models here.
--- empresa123xy/principalztw/models.py
+++ empresa123xy/principalztw/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Voos(models.Model):
+    destino = models.CharField(max_length=30)
+    horario = models.CharField(max_length=20)
+    valorIda = models.FloatField(max_length=30)
+    valorVolta = models.FloatField(max_length=20)
+    companhia = models.CharField(max_length=30)
+
+class Hotel(models.Model):
+    Nome = models.CharField(max_length=20)
+    CheckIn = models.DateField(max_length=30)
+    CheckOut = models.DateField(max_length=20)
+    valorHospedagem = models.FloatField(max_length=30)
+    companhia = models.CharField(max_length=20)
